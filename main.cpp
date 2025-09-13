e4e3cac4671a86735375aa72d9388e1fc4d28780
Isen Naiken <isen.naiken@nexomedis.com>
2025-09-13 14:51:56 +0200

    retry with gpt

--- main.cpp
+++ main.cpp
@@ -1,15 +1,12 @@
 #include <QApplication>
-#include <QMainWindow>
-#include "ui_mainwindow.h" // généré automatiquement par uic à partir de mainwindow.ui
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    QMainWindow window;
-    Ui::MainWindow ui;
-    ui.setupUi(&window); // initialise la fenêtre avec le contenu du .ui
-    window.show();
+    MainWindow w;
+    w.show();
 
     return app.exec();
 }
